b15c89376e9293415bdabf1aee869184954af5cb
Johnny Ngo <jtn.tria@gmail.com>
2024-09-17 01:11:39 -0500

    Draws frame of 4 triangles

--- src/Window.java
+++ src/Window.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.awt.geom.Path2D;
 
 public class Window {
 
@@ -9,6 +12,29 @@ public class Window {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pane.setLayout(new BorderLayout());
 
+        //Creating 4 triangle objects
+        List<Triangle> tris = new ArrayList<>();
+        //Face 1
+        tris.add(new Triangle(new Vertex(100, 100, 100),
+                                new Vertex(-100, -100, 100),
+                                new Vertex(-100, 100, -100),
+                                Color.white));
+        //Face 2
+        tris.add(new Triangle(new Vertex(100, 100, 100),
+                                new Vertex(-100, -100, 100),
+                                new Vertex(100, -100, -100),
+                                Color.RED));
+        //Face 3
+        tris.add(new Triangle(new Vertex(-100, 100, -100),
+                                new Vertex(100, -100, -100),
+                                new Vertex(100, 100, 100),
+                                Color.GREEN));
+        //Face 4
+        tris.add(new Triangle(new Vertex(-100, 100, -100),
+                                new Vertex(100, -100, -100),
+                                new Vertex(-100, -100, 100),
+                                Color.BLUE));
+
         //Create a render panel
         JPanel renderPanel = new JPanel() {
             public void paintComponent(Graphics g) {
@@ -17,6 +43,17 @@ public class Window {
                 g2.fillRect(0, 0, getWidth(), getHeight());
 
                 //Rendering happens here
+                //The generated shape is centered on the origin (0, 0, 0), and we will do a rotation around the origin later
+                g2.translate(getWidth() / 2, getHeight() / 2);
+                g2.setColor(Color.WHITE);
+                for(Triangle t : tris) {
+                    Path2D path = new Path2D.Double();
+                    path.moveTo(t.v1.x, t.v1.y);
+                    path.lineTo(t.v2.x, t.v2.y);
+                    path.lineTo(t.v3.x, t.v3.y);
+                    path.closePath();
+                    g2.draw(path);
+                }
             }
         };
 
