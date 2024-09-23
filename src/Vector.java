cd397da60f831de3b9f230f8723cc6685d32402b
Johnny Ngo <jtn.tria@gmail.com>
2024-09-23 00:23:43 -0500

    start of first stage of refactoring

--- src/Vector.java
+++ src/Vector.java
@@ -1,71 +1,77 @@
 public class Vector {
-    int direction[] = new int[3]; //(x, y, z)
-    int length = 3;
+    int x;
+    int y;
+    int z;
 
     Vector() {
-        for(int i = 0; i < length; i++) {
-            this.direction[i] = 0;
-        }
+        this.x = 0;
+        this.y = 0;
+        this.z = 0;
     }
 
     Vector(int x, int y, int z) {
-        this.direction[0] = x;
-        this.direction[1] = y;
-        this.direction[2] = z;
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
+    //Vector addition
     Vector addVectorToVector(Vector u) {
-        for(int i = 0; i < length; i++) {
-            this.direction[i] += u.direction[i];
-        }
+        this.x += u.x;
+        this.y += u.y;
+        this.z += u.z;
 
         return this;
     }
 
+    //Vector subtraction
     Vector subtractVectorFromVector(Vector u) {
-        for(int i = 0; i < length; i++) {
-            this.direction[i] -= u.direction[i];
-        }
+        this.x -= u.x;
+        this.y -= u.y;
+        this.z -= u.z;
 
         return this;
     }
 
+    //Multiplying a 1x3 vector by { {cos(theta), -sin(theta), 0}, {sin(theta), cos(theta), 0}, {0, 0, 1}}
     Vector rotateXYPlane(int angle) {
         Vector result = new Vector();
         double angleRad = Math.toRadians(angle);
-        result.direction[0] = (int)(Math.cos(angleRad) * this.direction[0] + -Math.sin(angleRad) * this.direction[1]);
-        result.direction[1] = (int)(Math.sin(angleRad) * this.direction[0] + Math.cos(angleRad) * this.direction[1]);
-        result.direction[2] = 1 * this.direction[2];
+        result.x = (int)(Math.cos(angleRad) * this.x + -Math.sin(angleRad) * this.y);
+        result.y = (int)(Math.sin(angleRad) * this.x + Math.cos(angleRad) * this.y);
+        result.z= 1 * this.z;
 
         return result;
     }
 
+    //Multiplying a 1x3 vector by { {cos(theta), 0, -sin(theta)}, {0, 1, 0}, {-sin(theta), 0, cos(theta)}}
     Vector rotateXZPlane(int angle) {
         Vector result = new Vector();
         double angleRad = Math.toRadians(angle);
-        result.direction[0] = (int)(Math.cos(angleRad) * this.direction[0] + -Math.sin(angleRad) * this.direction[2]);
-        result.direction[1] = 1 * this.direction[1];
-        result.direction[2] = (int)(-Math.sin(angleRad) * this.direction[0] + Math.cos(angleRad) * this.direction[2]);
+        result.x = (int)(Math.cos(angleRad) * x + -Math.sin(angleRad) * this.z);
+        result.y = 1 * this.y;
+        result.z = (int)(-Math.sin(angleRad) * x + Math.cos(angleRad) * this.z);
 
         return result;
     }
 
+    //Multiplying a 1x3 vector by { {1, 0, 0}, {0, cos(theta), -sin(theta)}, {0, sin(theta), cos(theta)}}
     Vector rotateYZPlane(int angle) {
         Vector result = new Vector();
         double angleRad = Math.toRadians(angle);
 
-        result.direction[0] = 1 * this.direction[0];
-        result.direction[1] = (int)(Math.cos(angleRad) * this.direction[1] + -Math.sin(angleRad) * this.direction[2]);
-        result.direction[2] = (int)(Math.sin(angleRad) * this.direction[1] + Math.cos(angleRad) * this.direction[2]);
+        result.x = 1 * this.x;
+        result.y = (int)(Math.cos(angleRad) * this.y + -Math.sin(angleRad) * this.z);
+        result.z = (int)(Math.sin(angleRad) * this.y + Math.cos(angleRad) * this.z);
 
         return result;
     }
 
     Vector scaleBy(int x, int y, int z) {
         Vector result = new Vector();
-        result.direction[0] = this.direction[0] * x;
-        result.direction[1] = this.direction[1] * y;
-        result.direction[2] = this.direction[2] * z;
+        result.x = this.x * x;
+        result.y = this.y * y;
+        result.y = this.z * z;
 
         return result;
     }
